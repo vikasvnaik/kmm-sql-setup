9ebd36ccaa35c6f3e2878e765d743aaa1cd1682d
vikas_n <vikas.naik@bolt.earth>
2024-01-12 11:07:49 +0530

    write/read data from sqldelight

--- shared/src/commonMain/kotlin/com/vikas/kmm/AppDao.kt
+++ shared/src/commonMain/kotlin/com/vikas/kmm/AppDao.kt
@@ -2,29 +2,11 @@ package com.vikas.kmm
 
 import com.vikas.kmm.data.Database
 import com.vikas.kmm.db.Hello
-import io.ktor.client.features.json.JsonFeature
-import io.ktor.client.features.json.serializer.KotlinxSerializer
-import io.ktor.client.features.logging.LogLevel
-import io.ktor.client.features.logging.Logger
-import io.ktor.client.features.logging.Logging
-import io.ktor.http.ContentType.Application.Json
-import kotlinx.serialization.json.Json
 
 class AppDao(databaseDriverFactory: DatabaseDriverFactory) {
     var database = Database(databaseDriverFactory)
     private val platform: Platform = getPlatform()
 
-    private val httpClient = httpClient {
-        install(Logging) {
-            level = LogLevel.HEADERS
-            logger = object : Logger {
-                override fun log(message: String) {
-                    Napier.v(tag = "HTTP Client", message = message)
-                } } }
-        install(JsonFeature) {
-            val json = Json { ignoreUnknownKeys = true }
-            serializer = KotlinxSerializer(json)
-        } }.also { initLogger() }
     fun insertData() {
         database.insert("hello")
         database.insert("hello1")
